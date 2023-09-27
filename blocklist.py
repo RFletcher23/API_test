7c3677c59bdd53db77b3ae28e4b41a2d3a9e1eba
RFletcher23 <67928814+RFletcher23@users.noreply.github.com>
2023-09-27 14:26:37 -0700

    Delete blocklist.py

--- blocklist.py
+++
@@ -1,4 +0,0 @@
-# This file contins the blocklist of the JWT tokens. 
-# It will be imported by app and the logout resource so that tokens can be added to the blocklist when a user logs out.
-
-BLOCKLIST = set() #This is a set because we don't want duplicates
